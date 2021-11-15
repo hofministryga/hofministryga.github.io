5a43213a1b145e0ec278b652f8af0b570647f0d0
Davidah121 <42045018+Davidah121@users.noreply.github.com>
2021-11-14 21:29:40 -0500

    TestCommit

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@
 function loadData()
 {
     var request = new XMLHttpRequest();
-    const url = "https://github.com/Davidah121/6502EMU/raw/master/help.txt";
+    const url = "https://docs.google.com/uc?export=download&id=1l9LbESuW8qHf2cW2w7NHJZqtMdt6zohH";
     request.open('GET', url, true);
     request.send();
     request.responseType = 'text';
